25d82e482e59efdcd019cdb70f9f3f977d31003a
Alexandre Pellitero Rivero <apellitero@gradiant.org>
2017-05-19 20:39:40 +0200

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectId} = require ('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user')
+
+// var id = '591f379e72d024234f0fb55411';
+//
+// if(! ObjectId.isValid(id) ){
+//   return console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo){
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+var id = '591d82e983978bd14848ccb7';
+
+User.findById(id).then((user) => {
+  if(!user){
+    return console.log('Uable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2);
+}).catch((e) => console.log(e));
